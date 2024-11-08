77ca8571b3f1b47b43b4d43ee613f22af2ab1c4d
ldenti@udesa.edu.ar <ldenti@udesa.edu.ar>
2024-11-08 12:49:44 -0300

    arreglando errores

--- js/index.js
+++ js/index.js
@@ -1,33 +1,52 @@
-
 fetch('https://dummyjson.com/recipes')
   .then(function (response) {
-  return response.json();
+    return response.json();
   })
   .then(function (data) {
-  let recetas = data.recipes;
-  let cocina = "";
-  let characterList = document.querySelector(".recipe-list");
+    let recetas = data.recipes;
+    let cocina = "";
+    let characterList = document.querySelector(".recipe-list");
 
-  for (let i = 0; i < 10; i++) {
-    cocina += `
-    <article class="article-recetas">
-    <img src= ${recetas[i].image} alt=''>
-    <article class="recetas-info">
-    <h2>${recetas[i].name} </h2>
-    <p>${recetas[i].difficulty} </p>
-    <article class="detalle-recetas-boton">
-    <a href="recetas.html?id=${recetas[i].id}">Ver más</a>
-    </article>
-    </article>
-    </article>
-    `;
-  }
-  console.log(cocina);
-  characterList.innerHTML = cocina
+    for (let i = 0; i < 10; i++) {
+      cocina += `
+      <article class="article-recetas">
+        <img src="${recetas[i].image}" alt="">
+        <article class="recetas-info">
+          <h2>${recetas[i].name}</h2>
+          <p>${recetas[i].difficulty}</p>
+          <article class="detalle-recetas-boton">
+            <a href="recetas.html?id=${recetas[i].id}">Ver más</a>
+          </article>
+        </article>
+      </article>
+        `;
+    }
+    characterList.innerHTML = cocina;
   })
   .catch(function (error) {
-  console.log("error: ", error);
+    console.log("Error al obtener recetas:", error);
   });
+
+// Función para ver detalles de una receta específica
+function verDetalle(id) {
+  fetch(`https://dummyjson.com/recipes/${id}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let detalle = document.getElementById("detalleReceta");
+      detalle.style.display = "block";  // Mostrar el contenedor de detalles
+      detalle.innerHTML = `
+            <h2>${data.name}</h2>
+            <img src="${data.image}" alt="${data.name}" style="width: 100%; max-width: 300px;">
+            <p>Dificultad: ${data.difficulty}</p>
+            <p>Descripción: ${data.instructions}</p>
+        `;
+    })
+    .catch(function (error) {
+      console.log("Error al obtener los detalles de la receta:", error);
+    });
+}
 
 
 
