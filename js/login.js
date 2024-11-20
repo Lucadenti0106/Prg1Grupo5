9576d49759fa9e736b3cf552adbb4cc1e9524b09
emiliopini <epini@udesa.edu.ar>
2024-11-20 15:05:48 -0300

    modificaciones en el html y js del login, y correcion palabras

--- js/login.js
+++ js/login.js
@@ -1,43 +1,24 @@
-let formulario = document.querySelector(".contact-form");
+let formulario = document.querySelector("#registroForm");
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 
-let errorEmail= document.querySelector(".email");
-let errorPassword = document.querySelector(".password");
-
-let emptyErrorMessage = "Porfavor complete el campo mail";
-let passwordError = "Porfavor complete el campo contraseña";
-
+let emptyErrorMessage = "Por favor complete el campo email.";
+let passwordError = "Por favor complete el campo contraseña.";
 
 formulario.addEventListener("submit", function(event) {
-   
-    event.preventDefault();
-
-
-        
-    
+    event.preventDefault(); 
+    let erroresBool = false; 
     if (email.value == "") {
-        errorEmail.style.display = "block";
-        errorEmail.innerText = emptyErrorMessage;  
-        erroresBool = true;    
-    }
-    else {
-    
-        errorEmail.style.display = "none";
+        alert(emptyErrorMessage); 
+        erroresBool = true;
     }
 
     if (password.value == "") {
-        errorPassword.style.display = "block";
-        errorPassword.innerText = passwordError;  
-        erroresBool = true;    
-    }
-    else {
-    
-        errorPassword.style.display = "none";
-    }
-    
-    if(erroresBool == false){
-        this.submit() 
+        alert(passwordError); 
+        erroresBool = true;
     }
 
+    if (erroresBool == false) {
+        this.submit(); 
+    }
 });
