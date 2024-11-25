629f1bc7912bb0e0d08b8206183bc481c2cae1e5
emiliopini <epini@udesa.edu.ar>
2024-11-25 18:01:48 -0300

    Cambios en el registro html y js

--- js/registro.js
+++ js/registro.js
@@ -1,24 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-let formulario = document.querySelector(".contact-form");
-let email = document.querySelector("#email");
-let password = document.querySelector("#password");
+document.addEventListener("DOMContentLoaded", function () {
+    let formulario = document.querySelector("#registroForm");
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
 
+    let errorEmail = document.querySelector("#emailError");
+    let errorPassword = document.querySelector("#passwordError");
 
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let erroresBool = false;
 
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    let erroresBool = false; 
+        if (email.value == "") {
+            errorEmail.style.display = "block";
+            errorEmail.innerText = "Por favor complete el campo";
+            erroresBool = true;
+        } else {
+            errorEmail.style.display = "none";
+        }
 
-    if (email.value == "") { 
-        erroresBool = true;
-    }
+        if (password.value == "") {
+            errorPassword.style.display = "block";
+            errorPassword.innerText = "Por favor complete el campo";
+            erroresBool = true;
+        } else {
+            errorPassword.style.display = "none";
+        }
 
-    if (password.value == "") {
-        erroresBool = true;
-    }
-
-    if (erroresBool == false) {
-        this.submit(); 
-    }
-});
+        if (erroresBool == false) {
+            this.submit();
+        }
+    });
 });
