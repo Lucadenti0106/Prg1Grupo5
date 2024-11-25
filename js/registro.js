9c97bb0f668daedc907a6df192e34d21defd3df1
ldenti@udesa.edu.ar <ldenti@udesa.edu.ar>
2024-11-25 15:47:28 -0300

    arreglando ultimas lineas

--- js/registro.js
+++ js/registro.js
@@ -1,32 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let formulario = document.querySelector(".contact-form");
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
+document.addEventListener("DOMContentLoaded", function() {
+let formulario = document.querySelector(".contact-form");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
 
-    let errorEmail= document.querySelector(".email");
-    let errorPassword = document.querySelector(".password");
 
-    form.addEventListener("submit", function (event) {
-        let isValid = true;
 
-        if (email.value.trim() === "") {
-            emailError.style.display = "block";
-            isValid = false;
-        } else {
-            emailError.style.display = "none";
-        }
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    let erroresBool = false; 
 
-        if (password.value.trim() === "") {
-            passwordError.style.display = "block";
-            isValid = false;
-        } else {
-            passwordError.style.display = "none";
-        }
+    if (email.value == "") { 
+        erroresBool = true;
+    }
 
-        if (!isValid) {
-            event.preventDefault();
-        } else {
-            window.location.href = "login.html";
-        }
-    });
+    if (password.value == "") {
+        erroresBool = true;
+    }
+
+    if (erroresBool == false) {
+        this.submit(); 
+    }
+});
 });
