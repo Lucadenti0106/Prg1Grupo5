2d62d48748fa10dbcc6b2b02c9c0b03b709c4ac9
benitocolombo05 <bcolombo@udesa.edu.ar>
2024-11-27 10:06:53 -0300

    retoque final style

--- js/registro.js
+++ js/registro.js
@@ -1,33 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let formulario = document.querySelector("#registroForm");
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
+let formulario = document.querySelector("#registroForm");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
 
-    let errorEmail = document.querySelector("#emailError");
-    let errorPassword = document.querySelector("#passwordError");
+let errorEmail = document.querySelector("#emailError");
+let errorPassword = document.querySelector("#passwordError");
 
-    formulario.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let erroresBool = false;
+formulario.addEventListener("submit", function (event) {
+event.preventDefault();
+let erroresBool = false;
 
-        if (email.value == "") {
-            errorEmail.style.display = "block";
-            errorEmail.innerText = "Por favor complete el campo";
-            erroresBool = true;
-        } else {
-            errorEmail.style.display = "none";
-        }
+if (email.value == "") {
+    errorEmail.style.display = "block";
+    errorEmail.innerText = "Por favor complete el campo";
+    erroresBool = true;
+} else {
+    errorEmail.style.display = "none";
+}
 
-        if (password.value == "") {
-            errorPassword.style.display = "block";
-            errorPassword.innerText = "Por favor complete el campo";
-            erroresBool = true;
-        } else {
-            errorPassword.style.display = "none";
-        }
+if (password.value == "") {
+    errorPassword.style.display = "block";
+    errorPassword.innerText = "Por favor complete el campo";
+    erroresBool = true;
+} else {
+    errorPassword.style.display = "none";
+}
 
-        if (erroresBool == false) {
-            this.submit();
-        }
-    });
+if (erroresBool == false) {
+    this.submit();
+}
 });
